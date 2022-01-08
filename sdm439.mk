8336a210c396a174cba549de307d98a224b0fd1e
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-09 00:00:50 +0300

    sdm439-common: Added support for long screenshot

--- sdm439.mk
+++ sdm439.mk
@@ -107,6 +107,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# LongShot
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/com.asus.stitchimage.service-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/com.asus.stitchimage.service-hiddenapi-package-whitelist.xml \
+    $(LOCAL_PATH)/configs/com.asus.stitchimage-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/com.asus.stitchimage-hiddenapi-package-whitelist.xml      
+
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
@@ -121,6 +126,15 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     Snap
+
+PRODUCT_PACKAGES += \
+    CustomQSLonghsot    
+
+PRODUCT_PACKAGES += \
+    StitchImage
+
+PRODUCT_PACKAGES += \
+    StitchImageService
 
 PRODUCT_PACKAGES += \
     libwa_dof
@@ -367,7 +381,11 @@ PRODUCT_PACKAGES += \
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/privapp-permissions-huaqin.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-huaqin.xml
+    $(LOCAL_PATH)/permissions/privapp-permissions-huaqin.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-huaqin.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-com.asus.stitchimage.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.asus.stitchimage.service.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-com.asus.stitchimage.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.asus.stitchimage.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-com.customqs.lonshot.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.customqs.lonshot.xml \
+    $(LOCAL_PATH)/default-permissions/default-asus-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default-asus-permissions.xml
 
 # Wifi
 PRODUCT_PACKAGES += \
